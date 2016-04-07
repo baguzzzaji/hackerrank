4c059e56ed9d8221b46bc219fd85270cd4f3e68c
Bagus Aji Santoso <baguzzzaji@gmail.com>
2016-04-07 14:38:54 +0700

    plus minus solution working

---
+++ src/algorithm/plusminus.java
@@ -0,0 +1,35 @@
+package algorithm;
+
+import java.util.Scanner;
+
+
+public class plusminus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        double positive = 0;
+        double negative = 0;
+        double zero = 0;
+        double[] num = new double[N];
+
+        for (int i = 0; i < N; i++) {
+            num[i] = sc.nextDouble();
+        }
+
+        // Looking for positive, negative, and zero
+        for (int i = 0; i < N; i++) {
+            if (num[i] > 0) {
+                positive++;
+            } else if (num[i] < 0) {
+                negative++;
+            } else {
+                zero++;
+            }
+        }
+
+        // Print the fraction
+        System.out.println(positive / N);
+        System.out.println(negative / N);
+        System.out.println(zero / N);
+    }
+}
