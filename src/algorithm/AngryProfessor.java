a34ca40db1488c404a3f84a7375c5bd022e8e1b5
Bagus Aji Santoso <baguzzzaji@gmail.com>
2016-04-14 22:29:30 +0700

    Works for some test cases

---
+++ src/algorithm/AngryProfessor.java
@@ -0,0 +1,33 @@
+package algorithm;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/angry-professor
+ */
+public class AngryProfessor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int N = 0;
+            int K = 0;
+            int student = 0;
+            N = sc.nextInt();
+            K = sc.nextInt();
+
+            for (int j = 0; j < N; j++) {
+                if (sc.nextInt() <= 0) {
+                    student++;
+                }
+            }
+
+            if (student <= K) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
