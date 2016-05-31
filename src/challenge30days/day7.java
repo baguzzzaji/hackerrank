03c9cc306215924ce42bf06f32bb2aa1f71bf97d
Bagus Aji Santoso <baguzzzaji@gmail.com>
2016-05-31 15:35:11 +0700

    Baru disini ngerti perbedaan Scanner.next() dengan Scanner.nextLine() :3

---
+++ src/challenge30days/day7.java
@@ -0,0 +1,33 @@
+package challenge30days;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/30-dictionaries-and-maps
+ */
+public class day7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map phoneBook = new HashMap<>();
+        int n = scanner.nextInt();
+        String name, phoneNumber;
+
+        // input section
+        for (int i = 0; i < n; i++) {
+            name = scanner.next();
+            phoneNumber = scanner.next();
+            phoneBook.put(name, phoneNumber);
+        }
+
+        for (int i = 0; i < n; i++) {
+            name = scanner.next();
+            if (phoneBook.containsKey(name)) {
+                System.out.println(name + "=" + phoneBook.get(name));
+            } else {
+                System.out.println("Not found");
+            }
+        }
+    }
+}
